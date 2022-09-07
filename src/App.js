792fd66c80c33ecb0c1e55a30abaa6f8f8c704d6
duyixian1234 <duyixian1234@qq.com>
2022-09-07 16:00:34 +0800

    add button to change N

--- src/App.js
+++ src/App.js
@@ -2,27 +2,45 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
-  const N = 9;
+  const [N, setN] = React.useState(3);
   return (
     <div>
-      {new Array(N).fill(0).map((_, i) => {
-        return (
-          <div class="grid">
-            {new Array(N).fill(0).map((_, j) => (
-              <div
-                style={{
-                  fontSize: 40,
-                  textAlign: 'center',
-                  width: 80,
-                  height: 80,
-                }}
-              >
-                {i * N + j}
-              </div>
-            ))}
-          </div>
-        );
-      })}
+      <button
+        onClick={() => {
+          if (N > 9) return;
+          setN(N + 1);
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          if (N < 3) return;
+          setN(N - 1);
+        }}
+      >
+        -
+      </button>
+      <div class="grid">
+        {new Array(N).fill(0).map((_, i) => {
+          return (
+            <div>
+              {new Array(N).fill(0).map((_, j) => (
+                <div
+                  style={{
+                    fontSize: 40,
+                    textAlign: 'center',
+                    width: 80,
+                    height: 80,
+                  }}
+                >
+                  {i * N + j}
+                </div>
+              ))}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
